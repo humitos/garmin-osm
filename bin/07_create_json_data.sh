a5187689e2d896358c636a971d43e1264b86530f
Manuel Kaufmann <humitos@gmail.com>
2016-01-15 18:13:06 -0500

    Json for web fixed

--- bin/07_create_json_data.sh
+++ bin/07_create_json_data.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-DATA_JSON = /home/humitos/apps/argentinaenpython.com.ar/mapas-de-openstreetmap-para-garmin/osm-data.json
+DATA_JSON=/home/humitos/apps/argentinaenpython.com.ar/mapas-de-openstreetmap-para-garmin/osm-data.json
 
-echo '{"{{size}}": "[[size]]", "{{date}}": "[[date]]", "{{md5}}": "[[md5]]", "{{sha1}}": "[[sha1]]", "{{sha256}}": [[sha256]]}' > ${DATA_JSON}
+echo '{"{{size}}": "[[size]]", "{{date}}": "[[date]]", "{{md5}}": "[[md5]]", "{{sha1}}": "[[sha1]]", "{{sha256}}": "[[sha256]]"}' > ${DATA_JSON}
+
 
 rpl --backup "[[size]]" "`ls -lh gmapsupp.img | awk '{printf $5, " "'}`" ${DATA_JSON} 
+
 rpl --backup "[[date]]" "`date +%d-%m-%y`" ${DATA_JSON} 
-rpl --backup "[[md5]]" "`cat gmapsupp.img.md5 | awk '{printf $1, " "'}`" ${DATA_JSON} 
-rpl --backup "[[sha1]]" "`cat gmapsupp.img.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON} 
-rpl --backup "[[sha256]]" "`cat gmapsupp.img.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+
+rpl --backup "[[md5]]" "`tail -n 1 gmapsupp.img.md5 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+
+rpl --backup "[[sha1]]" "`tail -n 1 gmapsupp.img.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+
+rpl --backup "[[sha256]]" "`tail -n 1 gmapsupp.img.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON} 
