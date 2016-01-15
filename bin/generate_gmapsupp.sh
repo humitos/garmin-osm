f6660145f129bb8c622713111b7e06bd9efc5af4
Manuel Kaufmann <humitos@gmail.com>
2016-01-15 16:50:36 -0500

    Script to be ran by CRON

---
+++ bin/generate_gmapsupp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PAIS=peru
+
+cd ~/garmin-osm
+
+./bin/build_mkgmap.sh
+./bin/01_cleanosmdata.sh
+./bin/02_getosmdata.sh ${PAIS}
+./bin/03_boundaries.sh ${PAIS}
+./bin/04_splitter.sh ${PAIS}
+./bin/05_mkgarminmap.sh ${PAIS}
+./bin/06_gensum.sh ${PAIS}
+./bin/07_create_json_data.sh ${PAIS}
+
+mv gmapsupp.img /home/humitos/apps/argentinaenpython.com.ar/mapas-de-openstreetmap-para-garmin/
