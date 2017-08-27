0179b02e479c9ecbdbd6afe1741fd81c196ce89c
Manuel Kaufmann <humitos@gmail.com>
2017-08-27 10:21:01 -0500

    Use --ignore-case for `rpl` command

--- bin/07_create_json_data.sh
+++ bin/07_create_json_data.sh
@@ -2,16 +2,17 @@
 
 PAIS=$1
 DATA_JSON=/home/humitos/apps/argentinaenpython.com.ar/mapas-de-openstreetmap-para-garmin/${PAIS}_osm-data.json
+GMAPSUPP=../gmapsupp.img
 
 echo '{"{{'${PAIS}'_size}}": "[[size]]", "{{'${PAIS}'_date}}": "[[date]]", "{{'${PAIS}'_md5}}": "[[md5]]", "{{'${PAIS}'_sha1}}": "[[sha1]]", "{{'${PAIS}'_sha256}}": "[[sha256]]"}' > ${DATA_JSON}
 
 
-rpl --backup "[[size]]" "`ls -lh gmapsupp.img | awk '{printf $5, " "'}`" ${DATA_JSON} 
+rpl --ignore-case --backup "[[size]]" "`ls -lh ${GMAPSUPP} | awk '{printf $5, " "'}`" ${DATA_JSON}
 
-rpl --backup "[[date]]" "`date +%d-%m-%y`" ${DATA_JSON} 
+rpl --ignore-case --backup "[[date]]" "`date +%d-%m-%y`" ${DATA_JSON}
 
-rpl --backup "[[md5]]" "`tail -n 1 gmapsupp.img.md5 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+rpl --ignore-case --backup "[[md5]]" "`tail -n 1 ${GMAPSUPP}.md5 | awk '{printf $1, " "'}`" ${DATA_JSON}
 
-rpl --backup "[[sha1]]" "`tail -n 1 gmapsupp.img.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+rpl --ignore-case --backup "[[sha1]]" "`tail -n 1 ${GMAPSUPP}.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON}
 
-rpl --backup "[[sha256]]" "`tail -n 1 gmapsupp.img.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON} 
+rpl --ignore-case --backup "[[sha256]]" "`tail -n 1 ${GMAPSUPP}.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON}
