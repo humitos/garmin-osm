83d02c76c41d2ee554450620a3d4ac5d9da71012
Manuel Kaufmann <humitos@gmail.com>
2017-08-27 10:23:31 -0500

    Use --quite to avoid problem with `raw_input` bug of rpl

--- bin/07_create_json_data.sh
+++ bin/07_create_json_data.sh
@@ -8,12 +8,12 @@ GMAPSUPP=${WORKDIR}/gmapsupp.img
 echo '{"{{'${PAIS}'_size}}": "[[size]]", "{{'${PAIS}'_date}}": "[[date]]", "{{'${PAIS}'_md5}}": "[[md5]]", "{{'${PAIS}'_sha1}}": "[[sha1]]", "{{'${PAIS}'_sha256}}": "[[sha256]]"}' > ${DATA_JSON}
 
 
-rpl --ignore-case --backup "[[size]]" "`ls -lh ${GMAPSUPP} | awk '{printf $5, " "'}`" ${DATA_JSON}
+rpl --quiet --ignore-case --backup "[[size]]" "`ls -lh ${GMAPSUPP} | awk '{printf $5, " "'}`" ${DATA_JSON}
 
-rpl --ignore-case --backup "[[date]]" "`date +%d-%m-%y`" ${DATA_JSON}
+rpl --quiet --ignore-case --backup "[[date]]" "`date +%d-%m-%y`" ${DATA_JSON}
 
-rpl --ignore-case --backup "[[md5]]" "`tail -n 1 ${GMAPSUPP}.md5 | awk '{printf $1, " "'}`" ${DATA_JSON}
+rpl --quiet --ignore-case --backup "[[md5]]" "`tail -n 1 ${GMAPSUPP}.md5 | awk '{printf $1, " "'}`" ${DATA_JSON}
 
-rpl --ignore-case --backup "[[sha1]]" "`tail -n 1 ${GMAPSUPP}.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON}
+rpl --quiet --ignore-case --backup "[[sha1]]" "`tail -n 1 ${GMAPSUPP}.sha1 | awk '{printf $1, " "'}`" ${DATA_JSON}
 
-rpl --ignore-case --backup "[[sha256]]" "`tail -n 1 ${GMAPSUPP}.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON}
+rpl --quiet --ignore-case --backup "[[sha256]]" "`tail -n 1 ${GMAPSUPP}.sha256 | awk '{printf $1, " "'}`" ${DATA_JSON}
